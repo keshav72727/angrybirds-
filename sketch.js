6ecba08986e1c97535882a9749158fc349b57865
keshav72727 <keshavbansal1406@gmail.com>
2021-01-19 16:44:54 +0530

    file uploaded successfully

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
 var platform;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,9 +38,19 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    
+    log6 = new Log(230,180,80,PI/2);
+    
     bird = new Bird(100,100);
 
+    var options ={
+        bodyA:bird.body,
+        bodyB:log6.body,
+        stiffness:0.04,
+        length:10
+    }
+    chain = Constraint.create(options);
+    World.add(world,chain);
 }
 
 function draw(){
@@ -61,6 +74,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    log6.display();
     bird.display();
+
+    stroke("black");
+    strokeWeight(4)
+    line(bird.body.position.x,bird.body.position.y,log6.body.position.x,log6.body.position.y)
 }
